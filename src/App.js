ba50a63c20752c8a04c50645ad525dd97a15a9c8
Nikoloz Tchavtchanidze <45031920+nchavchanidze@users.noreply.github.com>
2021-07-17 20:20:31 +0400

    React & GSAP - Part 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { TweenMax, Power3 } from "gsap";
+import userEvent from "@testing-library/user-event";
 
 function App() {
+  let logoItem = useRef(null)
+  let textItem = useRef(null)
+
+  useEffect(() => {
+    console.log(logoItem)
+    TweenMax.to(
+      logoItem,
+      .8,
+      {
+        opacity: 1,
+        y: -20,
+        ease: Power3.easeOut
+      }
+    )
+    TweenMax.to(
+      textItem,
+      .8,
+      {
+        opacity: 1,
+        y: -20,
+        ease: Power3.easeOut,
+        delay: .2
+      }
+    )
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img ref={el => {logoItem = el}} src={logo} className="App-logo" alt="logo" />
+        <p ref={el => {textItem = el}}>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
