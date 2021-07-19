29d6da05b995ca1c645938728943adc170e9997e
Nikoloz Tchavtchanidze <45031920+nchavchanidze@users.noreply.github.com>
2021-07-19 12:41:40 +0400

    React & GSAP - Slider

--- src/App.js
+++ src/App.js
@@ -1,31 +1,212 @@
-import React, { useRef, useEffect } from "react"
+import React, { useEffect, useRef, useState } from "react"
+import { TweenLite, Power3 } from "gsap"
+
+import leftArrow from "./img/arrow-left.svg"
+import rightArrow from "./img/arrow-right.svg"
+import image from "./img/image.jpg"
+import image2 from "./img/image2.jpg"
+import image3 from "./img/image3.jpg"
+
 import "./App.scss"
-import People from "./img/people.jpg"
-import CSSRulePlugin from "gsap/CSSRulePlugin"
-import { TimelineLite, Power2 } from "gsap"
 
-const App = () => {
-  let container = useRef(null)
-  let image = useRef(null)
-  let imageReveal = CSSRulePlugin.getRule(".img-container:after")
+const testimonials = [
+  {
+    name: "Julia Cameron",
+    title: "Creative Director, VISA",
+    image: image3,
+    quote:
+      "It's all good. I was amazed at the quality of the Design. We've seen amazing results already.",
+  },
+  {
+    name: "Mark Jacobs",
+    title: "Tech Lead, Google",
+    image: image,
+    quote:
+      "The rebranding has really helped our business. Definitely worth the investment.",
+  },
+  {
+    name: "Lisa Bearings",
+    title: "Brand Coordinator, Facebook",
+    image: image2,
+    quote:
+      "The service was excellent. Absolutely wonderful! A complete redesign did it for us.",
+  },
+]
 
-  const tl = new TimelineLite()
+function App() {
+  let imageList = useRef(null)
+  let testimonialList = useRef(null)
+
+  const imageWidth = 340
+
+  const [state, setState] = useState({
+    isActive1: true,
+    isActive2: false,
+    isActive3: false,
+  })
 
   useEffect(() => {
-    tl.to(container, 0, { css: { visibility: "visible" } })
-      .to(imageReveal, 1.4, { width: "0%", ease: Power2.easeInOut })
-      .from(image, 1.4, { scale: 1.6, ease: Power2.easeInOut, delay: -1.6 })
-  })
+    TweenLite.to(testimonialList.children[0], 0, {
+      opacity: 1,
+    })
+  }, [])
+
+  const slideLeft = (index, duration, multiplied = 1) => {
+    TweenLite.to(imageList.children[index], duration, {
+      x: -imageWidth * multiplied,
+      ease: Power3.easeOut,
+    })
+  }
+  const slideRight = (index, duration, multiplied = 1) => {
+    TweenLite.to(imageList.children[index], duration, {
+      x: imageWidth * multiplied,
+      ease: Power3.easeOut,
+    })
+  }
+
+  const scale = (index, duration) => {
+    TweenLite.from(imageList.children[index], duration, {
+      scale: 1.2,
+      ease: Power3.easeOut,
+    })
+  }
+
+  const fadeOut = (index, duration) => {
+    TweenLite.to(testimonialList.children[index], duration, {
+      opacity: 0,
+    })
+  }
+  const fadeIn = (index, duration) => {
+    TweenLite.to(testimonialList.children[index], duration, {
+      opacity: 1,
+      delay: 1,
+    })
+  }
+
+  const nextSlide = () => {
+    if (imageList.children[0].classList.contains("active")) {
+      setState({ isActive1: false, isActive2: true })
+
+      slideLeft(0, 1)
+      slideLeft(1, 1)
+      scale(1, 1)
+
+      fadeOut(0, 1)
+      fadeIn(1, 1)
+    } else if (imageList.children[1].classList.contains("active")) {
+      setState({ isActive2: false, isActive3: true })
+
+      slideRight(0, 1)
+      slideLeft(1, 1, 2)
+      slideLeft(2, 1, 2)
+      scale(2, 1)
+
+      fadeOut(1, 1)
+      fadeIn(2, 1)
+    } else if (imageList.children[2].classList.contains("active")) {
+      setState({ isActive1: true, isActive3: false })
+
+      slideLeft(2, 1, 3)
+      slideLeft(0, 1, 0)
+      slideLeft(1, 0, 0)
+      scale(0, 1)
+
+      fadeOut(2, 1)
+      fadeIn(0, 1)
+    }
+  }
+
+  const prevSlide = () => {
+    if (imageList.children[0].classList.contains("active")) {
+      setState({ isActive1: false, isActive3: true })
+
+      slideLeft(2, 0, 3)
+      slideLeft(2, 1, 2)
+      scale(2, 1)
+      slideRight(0, 1)
+      slideRight(1, 1)
+
+      fadeOut(0, 1)
+      fadeIn(2, 1)
+    } else if (imageList.children[1].classList.contains("active")) {
+      setState({ isActive2: false, isActive1: true })
+
+      slideLeft(0, 0)
+      slideRight(0, 1, 0)
+      slideRight(1, 1, 0)
+      slideRight(2, 1, 2)
+      scale(0, 1)
+
+      fadeOut(1, 1)
+      fadeIn(0, 1)
+    } else if (imageList.children[2].classList.contains("active")) {
+      setState({ isActive2: true, isActive3: false })
+
+      slideLeft(2, 1)
+      slideLeft(1, 0, 2)
+      slideLeft(1, 1)
+      scale(1, 1)
+
+      fadeOut(2, 1)
+      fadeIn(1, 1)
+    }
+  }
+
   return (
-    <section className="main">
-      <div ref={(el) => (container = el)} className="container">
-        <>
-          <div className="img-container">
-            <img ref={(el) => (image = el)} src={People} alt="people" />
+    <div className="testimonial-section">
+      <div className="testimonial-container">
+        <div onClick={prevSlide} className="arrows left">
+          <span>
+            <img src={leftArrow} alt="left arrow" />
+          </span>
+        </div>
+        <div className="inner">
+          <div className="t-image">
+            <ul ref={(el) => (imageList = el)}>
+              <li className={state.isActive1 ? "active" : ""}>
+                <img src={testimonials[0].image} alt={testimonials[0].name} />
+              </li>
+              <li className={state.isActive2 ? "active" : ""}>
+                <img src={testimonials[1].image} alt={testimonials[1].name} />
+              </li>
+              <li className={state.isActive3 ? "active" : ""}>
+                <img src={testimonials[2].image} alt={testimonials[2].name} />
+              </li>
+            </ul>
           </div>
-        </>
+          <div className="t-content">
+            <ul ref={(el) => (testimonialList = el)}>
+              <li className={state.isActive1 ? "active" : ""}>
+                <div className="content-inner">
+                  <p className="quote">{testimonials[0].quote}</p>
+                  <h3 className="name">{testimonials[0].name}</h3>
+                  <h4 className="title">{testimonials[0].title}</h4>
+                </div>
+              </li>
+              <li className={state.isActive2 ? "active" : ""}>
+                <div className="content-inner">
+                  <p className="quote">{testimonials[1].quote}</p>
+                  <h3 className="name">{testimonials[1].name}</h3>
+                  <h4 className="title">{testimonials[1].title}</h4>
+                </div>
+              </li>
+              <li className={state.isActive3 ? "active" : ""}>
+                <div className="content-inner">
+                  <p className="quote">{testimonials[2].quote}</p>
+                  <h3 className="name">{testimonials[2].name}</h3>
+                  <h4 className="title">{testimonials[2].title}</h4>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div onClick={nextSlide} className="arrows right">
+          <span>
+            <img src={rightArrow} alt="right arrow" />
+          </span>
+        </div>
       </div>
-    </section>
+    </div>
   )
 }
 
